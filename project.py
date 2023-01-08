8dd464d95a4186e580d31ab1b3e5b00150a7df75
GusPietrasanta <gusdsm@gmail.com>
2023-01-08 21:22:56 +1030

    First comments, structure

---
+++ project.py
@@ -0,0 +1,25 @@
+def main():
+    ...
+
+
+# Validate user input
+# Use argparse library and complete usage text output
+def function_1():
+    ...
+
+# List of countries first maybe?
+# List of cities first maybe?
+
+
+# Call Geocoding API and parse information
+def function_2():
+    ...
+
+
+# Call weather API and parse information
+def function_n():
+    ...
+
+
+if __name__ == "__main__":
+    main()
