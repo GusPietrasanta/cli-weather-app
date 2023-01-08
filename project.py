772d829b4ef7ea2efc2ea34b02a3ba5f0bd31f79
GusPietrasanta <gusdsm@gmail.com>
2023-01-09 00:18:30 +1030

    First test getting latitude and longitude

--- project.py
+++ project.py
@@ -4,9 +4,24 @@ import geonamescache
 
 # https://pypi.org/project/geonamescache/
 gc = geonamescache.GeonamesCache()
-buenos_aires = gc.get_cities_by_name("Buenos Aires")
-print(buenos_aires[0]['3435910']['latitude'])
-print(buenos_aires[0]['3435910']['longitude'])
+# 1 - Get city name from user
+# 2 - Get city code from city
+# 3 - Access to dictionary using city code
+# 4 - Get both latitude and longitude like this:
+
+# Get city:
+buenos_aires = gc.get_cities_by_name("Adelaide")
+
+# Get code:
+# print(buenos_aires[0].keys())
+buenos_aires_code = list(buenos_aires[0].keys())
+# print(list(buenos_aires_code)[0])
+
+# Get latitude and longitude:
+# print(buenos_aires[0]['3435910']['latitude'])
+# print(buenos_aires[0]['3435910']['longitude'])
+print(buenos_aires[0][buenos_aires_code[0]]['latitude'])
+print(buenos_aires[0][buenos_aires_code[0]]['longitude'])
 
 
 def main():
