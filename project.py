0d78983a966148717f903b033b649fc89333ed47
GusPietrasanta <gusdsm@gmail.com>
2023-01-10 09:31:24 +1030

    Getting user input and coordinates working

--- project.py
+++ project.py
@@ -17,21 +17,32 @@ countries_list = gc.get_countries_by_names()
 
 def main():
     print(generate_ascii_art())
-    city_name, city_code, index = get_user_input()
+
+    city_data = get_user_input()
+    city_name = city_data["name"]
+    city_code = city_data["geonameid"]
+    country_code = city_data["countrycode"]
+    latitude = city_data["latitude"]
+    longitude = city_data["longitude"]
+    country = get_country_name_from_code(country_code)
+
     print("City name: ", city_name)
     print("City code: ", city_code)
-    print("Latitude: ", get_latitude(city_name, city_code, index))
-    print("Longitude: ", get_longitude(city_name, city_code, index))
+    print("Country: ", country)
+    print("Latitude: ", latitude)
+    print("Longitude: ", longitude)
 
 
 # Validate user input
 def get_user_input():
     while True:
         user_input = input("What city would you like to know about the weather?: ")
-        similar_results = gc.search_cities(user_input, case_sensitive=False)
+        similar_results = gc.search_cities(user_input, case_sensitive=False, attribute="name")
+        similar_results = sorted(similar_results, key=lambda d: d["population"], reverse=True)
         if len(similar_results) > 1:
             # If more than one city was found, show options:
-            print("\nWell, we have a couple of coincidences here.\n")
+            print("\nWell, we have a couple of coincidences here.")
+            print("(Sorted by population)\n")
             option_lists = []
 
             for i in range(len(similar_results)):
@@ -44,15 +55,13 @@ def get_user_input():
 
             while True:
                 try:
-                    user_selection = int(input("\nWhich one is the one you are after? "))
+                    user_selection = int(input(f"\nWhich one is the one you are after? (0 to {len(similar_results)}): "))
                 except ValueError:
                     print("Not a valid option! Try again.")
                     continue
                 if user_selection in option_lists:
-                    city_name = similar_results[user_selection - 1]["name"]
-                    city_code = similar_results[user_selection - 1]["geonameid"]
-                    index = user_selection - 1
-                    return city_name, city_code, index
+                    city_data = similar_results[user_selection - 1]
+                    return city_data
                 else:
                     print("Not a valid option! Try again.")
 
@@ -63,16 +72,14 @@ def get_user_input():
             continue
         else:
             city_name = similar_results[0]["name"]
-            city_code = similar_results[0]["geonameid"]
             country_code = similar_results[0]["countrycode"]
+            city_data = similar_results[0]
             country = get_country_name_from_code(country_code)
             print()
-            # TODO: Convert country code to country name
             while True:
-                user_confirmation = input(f"Are we talking about {city_name}, {country}, {city_code} {flag.flag(country_code)}? (Y/N): ")
+                user_confirmation = input(f"Are we talking about {city_name}, {country} {flag.flag(country_code)}? (Y/N): ")
                 if user_confirmation.lower() == "y" or user_confirmation.lower() == "yes":
-                    index = 0
-                    return city_name, city_code, index
+                    return city_data
                 elif user_confirmation.lower() == "n" or user_confirmation.lower() == "no":
                     print("\nOk, let's try again then.")
                     break
@@ -85,30 +92,9 @@ def generate_ascii_art():
     ...
 
 
-def get_city_code(user_input):
-    ...
-
-
 def get_country_name_from_code(code):
     mapper = country(from_key="iso", to_key="name")
     return mapper(code)
-
-
-# Get latitude from city name
-def get_latitude(city_name, city_code, index):
-    city = gc.get_cities_by_name(city_name)
-    return (city[index][f"{city_code}"]["latitude"])
-
-
-# Get longitude from city name
-def get_longitude(city_name, city_code, index):
-    city = gc.get_cities_by_name(city_name)
-    return (city[index][f"{city_code}"]["longitude"])
-
-
-# Call Geocoding API and parse information
-def function_2():
-    ...
 
 
 # Call weather API and parse information
