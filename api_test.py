b7af5a097521799074923b7123971385b6d3fb62
GusPietrasanta <gusdsm@gmail.com>
2023-01-10 18:09:24 +1030

    API call and data parsing working perfectly.

--- api_test.py
+++ api_test.py
@@ -1,68 +1,50 @@
-from dotenv import load_dotenv
 import os
-from datetime import datetime
+import sys
 import emoji
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
 from project import get_country_name_from_code
 
 load_dotenv()
 
+latitude = "-34.92866"
+
+longitude = "138.59863"
+
 API_KEY = os.getenv("API_KEY")
 
-# print(API_KEY)
+units = "metric"
 
 
 # Weather now
 def main():
 
-    units = "metric"
-
     if units == "metric":
         temp_unit = "°C"
         speed_unit = "meters/sec"
         distance_unit = "kms"
-
     elif units == "imperial":
         temp_unit = "°F"
         speed_unit = "miles/hour"
         distance_unit = "miles"
+    else:
+        raise ValueError("Units were not properly defined.")
 
-    current_response = {
-        "coord": {"lon": 138.5986, "lat": -34.9287},
-        "weather": [
-            {"id": 800, "main": "Clear", "description": "clear sky", "icon": "01d"}
-        ],
-        "base": "stations",
-        "main": {
-            "temp": 29.51,
-            "feels_like": 28.85,
-            "temp_min": 25.22,
-            "temp_max": 31.65,
-            "pressure": 1012,
-            "humidity": 37,
-        },
-        "visibility": 5000,
-        "wind": {"speed": 7.2, "deg": 240},
-        "clouds": {"all": 0},
-        "dt": 1673318867,
-        "sys": {
-            "type": 1,
-            "id": 9566,
-            "country": "AU",
-            "sunrise": 1673293336,
-            "sunset": 1673344974,
-        },
-        "timezone": 37800,
-        "id": 2078025,
-        "name": "Adelaide",
-        "cod": 200,
-    }
+    api_url = generate_api_url(latitude, longitude, API_KEY, units)
+
+    try:
+        current_response = requests.get(api_url)
+    except requests.RequestException:
+        sys.exit("Oops!Something went wrong.")
+
+    current_response = current_response.json()
 
     if current_response["cod"] != 200:
-        print("Sorry! Something went wrong.")
+        sys.exit("Oops!Something went wrong.")
 
     else:
         timestamp = current_response["dt"]
-
         city_name = current_response["name"]
         country_code = current_response["sys"]["country"]
         country_name = get_country_name_from_code(country_code)
@@ -72,29 +54,22 @@ def main():
         description = current_response["weather"][0]["description"]
         sunrise = current_response["sys"]["sunrise"]
         sunset = current_response["sys"]["sunset"]
-        weather_emoji = get_emoji_from_id(weather_id,
-                                          timestamp,
-                                          sunrise,
-                                          sunset)
-
+        weather_emoji = get_emoji_from_id(weather_id, timestamp, sunrise, sunset)
         temp = current_response["main"]["temp"]
         feels_like = current_response["main"]["feels_like"]
         temp_min = current_response["main"]["temp_min"]
         temp_max = current_response["main"]["temp_max"]
         pressure = current_response["main"]["pressure"]
         humidity = current_response["main"]["humidity"]
-
         raw_visibility = current_response["visibility"]
-
         visibility = calculate_visibility(raw_visibility, units)
-
         wind_speed = current_response["wind"]["speed"]
         wind_direction = deg_to_compass(current_response["wind"]["deg"])
-
         clouds = current_response["clouds"]["all"]
-
+        dt = current_response["dt"]
         sunrise_print = datetime.fromtimestamp(sunrise).strftime("%I:%M:%S %p")
         sunset_print = datetime.fromtimestamp(sunset).strftime("%I:%M:%S %p")
+        last_updated = datetime.fromtimestamp(dt).strftime("%I:%M:%S %p")
 
         print(f"The current weather in {city_name}, {country_name} is:")
         print(emoji.emojize(f"{weather_emoji}   {main} ({description})"))
@@ -135,6 +110,7 @@ def main():
         print(emoji.emojize(f":cloud:   Cloudiness: {clouds}%"))
         print(emoji.emojize(f":sunrise:  Sunrise: {sunrise_print}"))
         print(emoji.emojize(f":sunset:  Sunset: {sunset_print}"))
+        print(emoji.emojize(f":hourglass_done:  Last updated: {last_updated}"))
 
 
 def get_emoji_from_id(id, timestamp, sunrise, sunset):
@@ -196,6 +172,11 @@ def calculate_visibility(raw_visibility, units):
         else:
             visibility = raw_visibility / 1000.00
     return round(visibility, 2)
+
+
+def generate_api_url(latitude, longitude, API_KEY, units):
+    return f"""https://api.openweathermap.org/data/2.5/weather?\
+lat={latitude}&lon={longitude}&appid={API_KEY}&units={units}"""
 
 
 if __name__ == "__main__":
